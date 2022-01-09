03332a4bec72224d066c86139c543cd937610d54
ErykGawel <eryk.gawel@wp.pl>
2022-01-09 12:46:58 +0100

    16 | register & login

--- mystie/settings.py
+++ mystie/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -138,3 +141,4 @@ MEDIA_URL = '/media/'
 # SECURE_HSTS_SECONDS = 3153600  # year
 # SECURE_HSTS_PRELOAD = True
 # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
